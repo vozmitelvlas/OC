d450e5478fc8ecda14299f68e10abe76dd059bcb
vozmitelvlas <vozmitel.vlas228@mail.ru>
2023-05-29 17:56:33 +0300

    2

--- inter/main.c
+++ inter/main.c
@@ -1,134 +1,77 @@
-#include <linux/gpio.h>
-#include <linux/interrupt.h>
+#include <linux/module.h>	
 #include <linux/kernel.h>
-#include <linux/module.h>
- 
-static int button_irqs[] = { -1, -1 };
- 
-/* Определение вводов-выводов для светодиодов.
- * Номера выводов можете изменить.
+#include <linux/sched.h>
+#include <linux/timer.h>
+#include <linux/init.h>
+#include <linux/gpio.h>
+
+#define LED1	4
+
+static struct timer_list blink_timer;
+
+/*
+ * Timer function called periodically
  */
-static struct gpio leds[] = { { 4, GPIOF_OUT_INIT_LOW, "LED 1" } };
- 
-/* Определение вводов-выводов для BUTTONS.
- * Номера вводов-выводов можете изменить.
+static void blink_timer_func(unsigned long data)
+{
+	printk(KERN_INFO "%s\n", __func__);
+
+	gpio_set_value(LED1, data); 
+	
+	/* schedule next execution */
+	blink_timer.data = !data;						// makes the LED toggle 
+	blink_timer.expires = jiffies + (1*HZ); 		// 1 sec.
+	add_timer(&blink_timer);
+}
+
+/*
+ * Module init function
  */
-static struct gpio buttons[] = { { 17, GPIOF_IN, "LED 1 ON BUTTON" },
-                                 { 18, GPIOF_IN, "LED 1 OFF BUTTON" } };
- 
-/* Функция обработки прерываний, активируемая нажатием кнопки. */
-static irqreturn_t button_isr(int irq, void *data)
+static int __init gpiomod_init(void)
 {
-    /* Первая кнопка. */
-    if (irq == button_irqs[0] && !gpio_get_value(leds[0].gpio))
-        gpio_set_value(leds[0].gpio, 1);
-    /* Вторая кнопка. */
-    else if (irq == button_irqs[1] && gpio_get_value(leds[0].gpio))
-        gpio_set_value(leds[0].gpio, 0);
- 
-    return IRQ_HANDLED;
+	int ret = 0;
+
+	printk(KERN_INFO "%s\n", __func__);
+
+	// register, turn off 
+	ret = gpio_request_one(LED1, GPIOF_OUT_INIT_LOW, "led1");
+
+	if (ret) {
+		printk(KERN_ERR "Unable to request GPIOs: %d\n", ret);
+		return ret;
+	}
+
+	/* init timer, add timer function */
+	init_timer(&blink_timer);
+
+	blink_timer.function = blink_timer_func;
+	blink_timer.data = 1L;							// initially turn LED on
+	blink_timer.expires = jiffies + (1*HZ); 		// 1 sec.
+	add_timer(&blink_timer);
+
+	return ret;
 }
- 
-static int __init intrpt_init(void)
+
+/*
+ * Module exit function
+ */
+static void __exit gpiomod_exit(void)
 {
-    int ret = 0;
- 
-    pr_info("%s\n", __func__);
- 
-    /* Регистрация вводов-выводов светодиодов. */
-    ret = gpio_request_array(leds, ARRAY_SIZE(leds));
- 
-    if (ret) {
-        pr_err("Unable to request GPIOs for LEDs: %d\n", ret);
-        return ret;
-    }
- 
-    /* Регистрация вводов-выводов для BUTTON. */
-    ret = gpio_request_array(buttons, ARRAY_SIZE(buttons));
- 
-    if (ret) {
-        pr_err("Unable to request GPIOs for BUTTONs: %d\n", ret);
-        goto fail1;
-    }
- 
-    pr_info("Current button1 value: %d\n", gpio_get_value(buttons[0].gpio));
- 
-    ret = gpio_to_irq(buttons[0].gpio);
- 
-    if (ret < 0) {
-        pr_err("Unable to request IRQ: %d\n", ret);
-        goto fail2;
-    }
- 
-    button_irqs[0] = ret;
- 
-    pr_info("Successfully requested BUTTON1 IRQ # %d\n", button_irqs[0]);
- 
-    ret = request_irq(button_irqs[0], button_isr,
-                      IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,
-                      "gpiomod#button1", NULL);
- 
-    if (ret) {
-        pr_err("Unable to request IRQ: %d\n", ret);
-        goto fail2;
-    }
- 
-    ret = gpio_to_irq(buttons[1].gpio);
- 
-    if (ret < 0) {
-        pr_err("Unable to request IRQ: %d\n", ret);
-        goto fail2;
-    }
- 
-    button_irqs[1] = ret;
- 
-    pr_info("Successfully requested BUTTON2 IRQ # %d\n", button_irqs[1]);
- 
-    ret = request_irq(button_irqs[1], button_isr,
-                      IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,
-                      "gpiomod#button2", NULL);
- 
-    if (ret) {
-        pr_err("Unable to request IRQ: %d\n", ret);
-        goto fail3;
-    }
- 
-    return 0;
- 
-/* Удаление проделанных настроек. */
-fail3:
-    free_irq(button_irqs[0], NULL);
- 
-fail2:
-    gpio_free_array(buttons, ARRAY_SIZE(leds));
- 
-fail1:
-    gpio_free_array(leds, ARRAY_SIZE(leds));
- 
-    return ret;
+	printk(KERN_INFO "%s\n", __func__);
+
+	// deactivate timer if running
+	del_timer_sync(&blink_timer);
+
+	// turn LED off
+	gpio_set_value(LED1, 0); 
+	
+	// unregister GPIO 
+	gpio_free(LED1);
 }
- 
-static void __exit intrpt_exit(void)
-{
-    int i;
- 
-    pr_info("%s\n", __func__);
- 
-    /* Свободные прерывания. */
-    free_irq(button_irqs[0], NULL);
-    free_irq(button_irqs[1], NULL);
- 
-    /* Отключение всех светодиодов. */
-    for (i = 0; i < ARRAY_SIZE(leds); i++)
-        gpio_set_value(leds[i].gpio, 0);
- 
-    /* Снятие регистрации. */
-    gpio_free_array(leds, ARRAY_SIZE(leds));
-    gpio_free_array(buttons, ARRAY_SIZE(buttons));
-}
- 
-module_init(intrpt_init);
-module_exit(intrpt_exit);
- 
+
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Handle some GPIO interrupts");
+MODULE_AUTHOR("Stefan Wendler");
+MODULE_DESCRIPTION("Basic kernel module using a timer and GPIOs to flash a LED.");
+
+module_init(gpiomod_init);
+module_exit(gpiomod_exit);
